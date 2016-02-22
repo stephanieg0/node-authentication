2deb3afbc2e168999c070c9bfae9ab0aaa131914
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-22 10:00:44 -0600

    added register verification

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -18,6 +21,22 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   res.redirect('/');
 });
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+
+  if (req.body.password === req.body.verify) {
+    res.redirect('/login');
+  } else {
+    res.render('register', {
+      message: 'Passwords do not match',
+      email: req.body.email});
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
